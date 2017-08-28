c078607dc5be9bff3875ea31eade8f91c31f0b57
Roxanne <argonkryptonneon@gmail.com>
2017-08-28 15:53:34 -0700

    we can add letters YAY

--- Spec/scrabble_spec.rb
+++ Spec/scrabble_spec.rb
@@ -8,21 +8,24 @@ require_relative '../Lib/scrabble.rb'
 
 describe "Scrabble" do
   describe "Scoring class" do
-    it "Can be created"
-    instance = Scrabble::Score.new
-    instance.must_be_a_kind_of Scrabble::Score
+
+    it "Can be created" do
+      instance = Scrabble::Scoring.new
+      instance.must_be_kind_of Scrabble::Scoring
+    end
+
+    it "has value for all letters" do
+      ("A".."Z").each do |letter|
+        instance = Scrabble::Scoring.new(letter)
+        instance.score.wont_equal 0
+      end
+    end
+
+    # it "Contains 26 key/value pairs" do
+    #   instance = Scrabble::Scoring.new
+    #   instance.point_per_letter.length.must_equal 27
+    # end
+
   end
-
-  it "the .point_per_letter can be called" do
-    instance = Scrabble::Score.new
-    instance.must_be_a_kind_of Hash
-  end
-
-  it "Contains 27 key/value pairs" do
-    instance = Scrabble::Score.new
-    instance.point_per_letter.length.must_equal 27
-  end
-
-
 
 end
