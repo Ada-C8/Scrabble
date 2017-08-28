0b405e1d2ca05c54cfce75834b32eea410b75490
IsabelDePapel <isuchanek@gmail.com>
2017-08-28 15:46:47 -0700

    Wrote and passed test for basic self.score

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,1 +1,20 @@
 require_relative 'spec_helper.rb'
+
+describe "Scoring class" do
+  describe "self.score" do
+    before do
+      @score = Scrabble::Scoring.score("cat")
+    end
+
+    it "Returns an integer from a string input" do
+      # cat is worth 5
+      @score.must_equal 5
+      @score.must_be_instance_of Integer
+
+    end
+
+    it "Adds a 50-point bonus if it's a seven-letter word (or longer)" do
+      
+    end
+  end
+end
