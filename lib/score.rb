c858415585324f78e9ec9dc80d88edc59ad1e55c
sairagula <abdukhalieva@gmail.com>
2017-08-28 15:31:18 -0700

    making sure lib is there

---
+++ lib/score.rb
@@ -0,0 +1,8 @@
+# require_relative 'lib/scrabble'
+module Scrabble
+  class Score
+    def initialize
+      @words = []
+    end
+  end
+end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -1,7 +1,15 @@
-module Scrabble
-  class Score
-    def initialize
-      @words = []
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require_relative '../lib/score'
+
+
+describe "Score" do
+  describe "#initialize" do
+    it " should give you an empty array" do
+      my_class = Scrabble::Score.new
+      my_class.wont_be_empty
     end
   end
 end
