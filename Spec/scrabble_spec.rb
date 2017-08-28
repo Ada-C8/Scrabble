443298b9c0f1613514d4074807744c9b1895ec3b
Eva Sawyer <esawyer25@gmail.com>
2017-08-28 15:17:13 -0700

    Added three tests

--- Spec/scrabble_spec.rb
+++ Spec/scrabble_spec.rb
@@ -7,18 +7,21 @@ require_relative '../Lib/scrabble.rb'
 
 
 describe "Scrabble" do
-
   describe "Scoring class" do
     it "Can be created"
     instance = Scrabble::Score.new
     instance.must_be_a_kind_of Scrabble::Score
   end
 
+  it "the .point_per_letter can be called" do
+    instance = Scrabble::Score.new
+    instance.must_be_a_kind_of Hash
+  end
+
   it "Contains 27 key/value pairs" do
     instance = Scrabble::Score.new
     instance.point_per_letter.length.must_equal 27
   end
-
 
 
 
