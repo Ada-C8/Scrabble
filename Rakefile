e59b4aca8477920cbb38a23de1f90834492f81c5
Eva Sawyer <esawyer25@gmail.com>
2017-08-28 15:16:49 -0700

    Made it work

--- Rakefile
+++ Rakefile
@@ -3,7 +3,7 @@ require 'rake/testtask'
 Rake::TestTask.new do |t|
   t.libs = ["Lib"]
   t.warning = true
-  t.test_files = FileList['Specs/*_spec.rb']
+  t.test_files = FileList['Spec/scrabble_spec.rb']
 end
 
 task default: :test
---
+++ Spec/scrabble_spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+
+require_relative '../Lib/scrabble.rb'
+
+
+describe "Scrabble" do
+
+  describe "Scoring class" do
+    it "Can be created"
+    instance = Scrabble::Score.new
+    instance.must_be_a_kind_of Scrabble::Score
+  end
+
+  it "Contains 27 key/value pairs" do
+    instance = Scrabble::Score.new
+    instance.point_per_letter.length.must_equal 27
+  end
+
+
+
+
+end
