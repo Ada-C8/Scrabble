fe9a9f819423cc64e6b7941b575c8eb76d4a6ec4
Bennett Rahn <rahnibennett@gmail.com>
2017-08-28 16:59:50 -0700

    write some code that sometimes works for highest_scoring_word, lots and lots of improper logic

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,3 +1,4 @@
+require 'pry'
 module Scrabble
 
   class Scoring
@@ -26,7 +27,7 @@ module Scrabble
 
     def self.score(word)
       total = 0
-      word.split('').each do |let|
+      word.upcase.split('').each do |let|
         total += score_letter(let)
       end
       if word.length == 7
@@ -35,6 +36,50 @@ module Scrabble
       return total
     end
 
+    def self.highest_score_from(words_arr)
+      highest_score = words_arr[0]
+      # tie = []
+      words_arr.each do |word|
+        if score(word) > score(highest_score)
+          highest_score = word
+        # elsif score(word) == score(highest_score)
+          # tie << word
+          # tie << highest_score
+        end
+      end
+      return highest_score.upcase
+
+
+      # this code doesn't work
+      # scorefinder = {}
+      # words_arr.each do |word|
+      #   scorefinder[word] = score(word)
+      # end
+      #
+      # highest_score = scorefinder.values.max
+      # highest_scoring_words = []
+      # highest_scoring_words << scorefinder.select { |k, v| v == highest_score }.keys
+      # binding.pry
+      # if highest_scoring_words.length == 0
+      #   return highest_scoring_words[0][0]
+      # end
+
+      #
+      # this is logic for determining ties
+      # binding.pry
+      # if tie
+      #   if
+      #     w1 = 7 letters
+      #     w1 wins
+      #   end
+      #   shortest word wins
+      # end
+
+      # return highest_score.upcase
+    end
+
   end
 
 end
+# Scrabble::Scoring.highest_score_from(["this", "that", "queasy"]).must_equal "queasy"
+# Scrabble::Scoring.highest_score_from(["gp", "dart", "dat" ])
