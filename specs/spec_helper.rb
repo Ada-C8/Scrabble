51fb835a9ae60afc0ba5fae4895b72414f543051
Isaac Del Rosario <cecdelr@gmail.com>
2017-08-28 15:52:59 -0700

    Set up spec and lib directories, created Rakefile and spec_helper.rb

---
+++ specs/spec_helper.rb
@@ -0,0 +1,20 @@
+# specs/spec_helper.rb
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
+
+require 'simplecov'
+SimpleCov.start
+
+# Require any classes
+# ex require_relative 'lib/foo.rb'
+require_relative 'lib/scoring.rb'
+
+# Uncomment below as you go through the waves
+require_relative 'wave-1-game.rb'
+# require_relative 'wave-2-game.rb'
+# require_relative 'wave-3-game.rb'
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
