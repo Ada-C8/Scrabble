73a42cfe6897a2440f5b3147d526ac7a6cae3db6
murog <guillermina.muro@gmail.com>
2017-08-28 15:32:13 -0700

    ok ready to go

---
+++ specs/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
+
+require 'simplecov'
+SimpleCov.start
+
+# Require any classes
+# ex require_relative 'lib/foo.rb'
+require_relative '../lib/scrabble.rb'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
