45aa4b18fcbdd65ce0f3ad01961db98134817e20
hellomarisamorris <hellomarisamorris@gmail.com>
2017-08-28 16:04:52 -0700

    test added 1 passing.

---
+++ lib/scrabble.rb
@@ -0,0 +1,8 @@
+module Scrabble
+  class Scoring
+
+    def self.score(input_word)
+    end
+
+  end#end of scoring
+end# end of module
--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -3,27 +3,41 @@ require_relative 'spec_helper'
 describe "score class " do
   # TODO: is this an instance???
   # it "new score is an instance of a score" do
-  #   input = Scrabble::Score.new(
-  #   input.must_be_instance_of Scrabble::Score
+  #   input = Scrabble::Scoring.new(
+  #   input.must_be_instance_of Scrabble::Scoring
   # end
-describe "self.score" do
-  it "should respond to score" do
-    # input = Scrabble::Score.new
-    (Scrabble::Score).must_respond_to :score
+  describe "self.score" do
+    it "should respond to score" do
+      # input = Scrabble::Scoring.new
+      (Scrabble::Scoring).must_respond_to :score
+    end
+    it "should return a total score for the word" do
+      output = Scrabble::Scoring.score("dog")
+      expected_output = 5
+      output.must_equal expected_output
+    end
+
   end
-  it "should return a total score for the word" do
-    output = Scrabble::Score("dog")
-    expected_output = 5
-    output.must_equal expected_output
-  end
-
-end
-
-describe "self.highest_score_from" do
-
-end
-
-describe "self.tie breaking rules" do
-
-end
+  #
+  # xdescribe "self.highest_score_from" do
+  #   it "Should respond to highest_score_from" do
+  #     (Scrable::Scoring).must_respond_to :highest_score_from
+  #   end
+  #
+  #   it "Should select highest_score_from Array" do
+  #     #Arrage
+  #     words = ["dog", "cat", "technical"]
+  #     expected_output = "technical"
+  #     # Action
+  #     output = Scrabble::Scoring(words)
+  #     # Assert
+  #     output.must_equal expected_output
+  #   end
+  # end
+  #
+  # xdescribe "self.tie breaking rules" do
+  #   #Arrage
+  #   # Action
+  #   # Assert
+  # end
 end
