194630d7414247cc8ebe7dc4ac4afb919c343782
Bennett Rahn <rahnibennett@gmail.com>
2017-08-28 15:31:59 -0700

    add letter_score logic

---
+++ lib/scoring.rb
@@ -0,0 +1,32 @@
+module Scrabble
+
+  class Scoring
+    def initialize
+
+    end
+
+    def self.score_letter(letter)
+      case letter
+      when 'A', 'E', 'I','O','U','L','N','R','S','T'
+        return 1
+      when 'D', 'G'
+        return 2
+      when 'B', 'C', 'M', 'P'
+        return 3
+      when 'F', 'H', 'V', 'W', 'Y'
+        return 4
+      when 'K'
+        return 5
+      when 'J', 'X'
+        return 8
+      when 'Q', 'Z'
+        return 10
+      end
+    end
+
+    def self.score(word)
+    end
+
+  end
+
+end
