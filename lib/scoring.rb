e13f98c605512657b25b739f9a07536df5fc2125
Sara C <srachandler@gmail.com>
2017-08-28 15:57:45 -0700

    Added data structures

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,13 +1,25 @@
 
 module Scrabble
   class Scoring
+
     def initialized
     end
 
+    def self.score(word)
 
-    
+    end
+
 
 
 
   end
 end
+
+
+point_one = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
+point_two = ["D", "G"]
+point_three = ["B", "C", "M", "P"]
+point_four = ["F", "H", "V", "W", "Y"]
+point_five = ["K"]
+point_eight = ["J", "X"]
+point_ten = ["Q", "Z"]
