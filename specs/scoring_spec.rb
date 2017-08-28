73c99109e28eab8f9d5e7db2e6729d0768587c50
Bennett Rahn <rahnibennett@gmail.com>
2017-08-28 15:32:33 -0700

    add tests for initialize and score_letter

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -4,3 +4,28 @@ require 'minitest/skip_dsl'
 require 'minitest/pride'
 
 require_relative 'spec_helper'
+
+describe "Scoring class" do
+  describe "initialize" do
+    it "can instantiate a scoring class" do
+      Scrabble::Scoring.new.must_be_kind_of Scrabble::Scoring
+    end
+  end
+  describe "score_letter" do
+    it "can take in letters worth 1 point and return" do
+      input =["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
+
+      input.each do |letter|
+        Scrabble::Scoring.score_letter(letter).must_equal 1
+      end
+    end
+
+    it "can take letters worth 4 and return 4" do
+      input =['F', 'H', 'V', 'W', 'Y']
+
+      input.each do |letter|
+        Scrabble::Scoring.score_letter(letter).must_equal 4
+      end
+    end
+  end
+end
