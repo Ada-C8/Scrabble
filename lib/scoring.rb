48b8808077e2665595cbbf0429c05673d1410383
Tanisha <tanishahampden@gmail.com>
2017-08-28 16:24:51 -0700

    Add test for self.score that returns that appropriate score for a single letter

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -2,5 +2,31 @@
 
 module Scrabble
   class ScoringClass
+    attr_reader :letter
+
+    def initialize
+      @letter = letter
+    end
+
+    def self.score(w)
+      case w
+      when "a", "e", "i", "o", "u", "l", "n", "r", "s", "t"
+        return 1
+      when "d", "g"
+        return 2
+      when "b", "c", "m", "p"
+        return 3
+      when "f", "h", "v", "w", "y"
+        return 4
+      when "j", "x"
+        return 8
+      when "k"
+        return 5
+      when "q", "z"
+        return 10
+      end
+
+
+    end
   end
 end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -15,6 +15,22 @@ describe "Scoring Class" do
       scoring.must_be_instance_of Scrabble::ScoringClass
     end
 
+    describe "score chart" do
+      #arrange
+      #act
+      # game = Scrabble::ScoringClass.new("s")
+      # letter_score = game.score
+
+      it "letter equal value" do
+        Scrabble::ScoringClass.score("a").must_equal 1
+        Scrabble::ScoringClass.score("d").must_equal 2
+        Scrabble::ScoringClass.score("b").must_equal 3
+        Scrabble::ScoringClass.score("f").must_equal 4
+        Scrabble::ScoringClass.score("k").must_equal 5
+        Scrabble::ScoringClass.score("j").must_equal 8
+        Scrabble::ScoringClass.score("q").must_equal 10
+      end
+    end
   end
   #arrange
   #act
