431f560fac624bb53272f93abf0c38dc9806a333
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-28 15:39:22 -0700

    add POINTS hash and define self.score method

--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -5,7 +5,23 @@ describe 'Scoring' do
     it "has a Constant variable that is a Hash" do
       Scrabble::Scoring::POINTS.must_be_kind_of Hash
     end
-  end
 
-  
+    it "can retrieve point value from POINTS hash" do
+      Scrabble::Scoring::POINTS["A"].must_equal 1
+      Scrabble::Scoring::POINTS["B"].must_equal 3
+      Scrabble::Scoring::POINTS["Z"].must_equal 10
+      Scrabble::Scoring::POINTS["K"].must_equal 5
+    end
+
+  end # 'initialize'
+
+  describe 'self.score' do
+
+    it "takes a word as an argument" do
+      Scrabble::Scoring.must_respond_to :score
+      Scrabble::Scoring.score("bird").must_be_kind_of Integer
+    end
+
+  end # 'self.score'
+
 end # end of Scoring
