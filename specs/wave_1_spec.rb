4442a7d5f915415486b22b38dcc58238ce26bd23
Rebecca Bergena <rbergena@gmail.com>
2017-08-28 15:49:13 -0700

    add score method and test

--- specs/wave_1_spec.rb
+++ specs/wave_1_spec.rb
@@ -7,14 +7,20 @@ require_relative '../lib/scrabble'
 describe 'Scrabble' do
   describe 'Scoring' do
 
+    before do
+      @game1 = Scrabble::Scoring.new
+    end
+
     it "Scoring.new is a new instance of the Scoring class" do
-      game1 = Scrabble::Scoring.new
-      game1.must_be_instance_of Scrabble::Scoring
+      @game1.must_be_instance_of Scrabble::Scoring
 
     end # new instance of scoring
 
-    it "self.score " do
-      
+    describe "self.score " do
+      it "CAT returns 5" do
+      Scrabble::Scoring.score("cAt").must_equal 5
+    end
+
     end # self.score
 
   end #describe scoring
