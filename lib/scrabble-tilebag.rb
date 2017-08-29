df6e450eba902b6636c946b482360c75e37a5713
averikitsch <aekitsch@gmail.com>
2017-08-29 14:42:58 -0700

    Added rest of tilebag methods and tests

--- lib/scrabble-tilebag.rb
+++ lib/scrabble-tilebag.rb
@@ -1,13 +1,61 @@
 
 module Scrabble
   class TileBag
-    attr_reader
+    attr_reader :tiles
 
     def initialize
       @tiles = {
-        "A" =>9,"B" =>2,"C" =>2,"D" =>4,"E" =>12,"F" =>2,"G" =>3,"H" =>2,"I" =>9,"J" =>1,"K" =>1,"L" =>4,
-        "M" =>2,"N" =>6,"O" =>8,"P" =>2,"Q" =>1,"R" =>6,"S" =>4,"T" =>6,"U" =>4,"V" =>2,"W" =>2,"X" =>1,
-        "Y" =>2,"Z" =>1}
+        "A" =>9,
+        "B" =>2,
+        "C" =>2,
+        "D" =>4,
+        "E" =>12,
+        "F" =>2,
+        "G" =>3,
+        "H" =>2,
+        "I" =>9,
+        "J" =>1,
+        "K" =>1,
+        "L" =>4,
+        "M" =>2,
+        "N" =>6,
+        "O" =>8,
+        "P" =>2,
+        "Q" =>1,
+        "R" =>6,
+        "S" =>4,
+        "T" =>6,
+        "U" =>4,
+        "V" =>2,
+        "W" =>2,
+        "X" =>1,
+        "Y" =>2,
+        "Z" =>1}
+    end
+
+    def draw_tiles(num)
+        hand = []
+        if tiles_remaining < num
+          num = tiles_remaining
+        end
+        until hand.length == num
+          letter = @tiles.keys.sample
+          if letter_remaining?(letter)
+            hand << letter
+            @tiles[letter] -= 1
+          end
+        end
+        return hand
+    end
+
+    def letter_remaining?(letter)
+        if @tiles[letter] > 0
+          return true
+        end
+    end
+
+    def tiles_remaining
+      return @tiles.values.inject(:+)
     end
 
   end
--- specs/scrabble_tilebag_specs.rb
+++ specs/scrabble_tilebag_specs.rb
@@ -1,10 +1,50 @@
 require_relative 'spec_helper.rb'
 require_relative '../lib/scrabble-player'
+require_relative '../lib/scrabble-tilebag'
 
 describe "Scrabble::TileBag" do
+  before do
+    @tiles = Scrabble::TileBag.new
+  end
   it "can be initialized" do
-    Scrabble::TileBag.new.must_be_instance_of Scrabble::TileBag
+    @tiles.must_be_instance_of Scrabble::TileBag
   end
-  it "can " do
+  it "initializes proper amount of tiles" do
+    @tiles.tiles.count.must_equal 26
   end
+  it "initializes proper tile amounts" do
+    @tiles.tiles["A"].must_equal 9
+    @tiles.tiles["Z"].must_equal 1
+  end
+  it "responds to draw_tiles" do
+    @tiles.must_respond_to :draw_tiles
+  end
+  it "returns a collection of tiles" do
+    drawn_tiles = @tiles.draw_tiles(2)
+    drawn_tiles.length.must_equal 2
+    drawn_tiles.must_be_instance_of Array
+  end
+  it "removes tiles from tilebag after draw_tiles" do
+    total_before = @tiles.tiles.inject(0){|sum, (k,v)| sum + v}
+    drawn_tiles = @tiles.draw_tiles(7)
+    total_after = @tiles.tiles.inject(0){|sum, (k,v)| sum + v}
+    total_after.must_equal (total_before - 7)
+  end
+  it "tiles can't go negative" do
+    14.times do
+      @tiles.draw_tiles(7)
+    end
+    @tiles.tiles_remaining.must_equal 0
+    @tiles.draw_tiles(1).must_equal []
+  end
+  it "tiles_remaining returns appropriate number" do
+    @tiles.draw_tiles(10)
+    @tiles.tiles_remaining.must_equal 88
+  end
+  it "titles_remaining will delete nothing if given zero" do
+    @tiles.draw_tiles(0)
+    @tiles.tiles_remaining.must_equal 98
+  end
+
+
 end
