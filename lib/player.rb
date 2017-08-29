25ddac05e4b235f074f33cb5a8c0e2dda87d518b
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-29 14:27:33 -0700

    add lib/player.rb and specs/player_spec.rb

---
+++ lib/player.rb
@@ -0,0 +1,26 @@
+module Scrabble
+  class Player
+
+    def initialize(name)
+      @name = name
+      @plays = []
+    end
+
+    def play(word)
+    end
+
+    def total_score
+    end
+
+    def highest_scoring_word
+    end
+
+    def highest_word_score
+    end
+
+    private
+    def won?
+    end
+
+  end # end of Player class
+end # end of Scrabble module
