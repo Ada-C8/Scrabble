83e5448bdd51adec894e4b74edef7ec93444215e
Jessica Owens <vertige@gmail.com>
2017-08-29 14:13:14 -0700

    added Player class files

---
+++ lib/player.rb
@@ -0,0 +1,6 @@
+module Scrabble
+  class Player
+
+  end #end of Player class
+
+end #end of module
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/scoring'
+require_relative '../lib/player'
 
 
 reporter_options = { color:true}
