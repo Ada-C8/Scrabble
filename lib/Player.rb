411dd295375dc04c3ce78d830ecade27fe3e9a97
Diane Lenertz <lenertzd@grinnell.edu>
2017-08-29 14:19:13 -0700

    outline Player class

---
+++ lib/Player.rb
@@ -0,0 +1,44 @@
+module Scrabble
+  class Player
+    #attr_reader :name, :plays
+
+    def initialize(name)
+      #@name = name
+      #@plays = []
+      #@total_score = 0
+    end
+
+    #plays returns an Array of the words played by the player
+
+    def play(word)
+      #adds input word to plays Array
+      #@plays << word
+      #returns false if the player has already won
+      #returns score of the word
+      #Scoring.score(word)
+      #total_score += score
+      #won?
+    end
+
+    def highest_word_score
+      #@plays.each do |play|
+        #if play.score
+        #...
+    end
+
+
+    private
+    def won?
+      #if score >100
+        #return true
+      #else
+        #return false
+    end
+
+
+
+
+
+
+  end
+end
