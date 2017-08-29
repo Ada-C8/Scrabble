d6416c43faedf3aca7fbf4bf5aeea32dab2a175d
IsabelDePapel <isuchanek@gmail.com>
2017-08-28 17:06:58 -0700

    Updated relative requires

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'minitest/skip_dsl'
 require_relative '../lib/scoring'
 
 
 reporter_options = { color:true}
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(reporter_options)
-
