fd380717f624215821e2dbf6e69c8a2694977e32
Kimberley Zell <kimberley.zell@gmail.com>
2017-08-29 15:05:04 -0700

    added some tests

--- Specs/wave-2-specs.rb
+++ Specs/wave-2-specs.rb
@@ -3,3 +3,29 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'pry'
+
+describe 'Player' do
+
+  describe "initialize" do
+    @new_player = Scrabble::Player.new("Ada")
+    it "must_be_instance_of Player" do
+      new_player = Scrabble::Player.new("Ada")
+      new_player.must_be_instance_of Player
+    end
+
+    it "must respond to method name" do
+      @new_player.name.must_equal "Ada"
+    end
+  end
+
+    describe "plays" do
+
+      it "must return an Array" do
+        @new_player.plays.must_be_kind_of Array
+      end
+
+      it
+
+    end
+
+end
