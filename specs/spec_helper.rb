1b3a818157220dc1b3d890b401410d3d9b9afdc2
Isaac Del Rosario <cecdelr@gmail.com>
2017-08-28 17:00:15 -0700

    Removed wave-.rb files, didn't need to put them in spec_helper.rb

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -11,10 +11,5 @@ SimpleCov.start
 # ex require_relative 'lib/foo.rb'
 require_relative '../lib/scoring.rb'
 
-# Uncomment below as you go through the waves
-# require_relative '../wave-1-game.rb'
-# require_relative '../wave-2-game.rb'
-# require_relative '../wave-3-game.rb'
-
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
