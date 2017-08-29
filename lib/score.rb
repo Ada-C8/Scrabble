3e8a84c15d37224aa7a632488a3f81dfb59a2f49
sairagula <abdukhalieva@gmail.com>
2017-08-29 13:53:59 -0700

    repeat pushing

--- lib/score.rb
+++ lib/score.rb
@@ -5,5 +5,34 @@ module Scrabble
     def initialize
       @words = []
     end
+
+    def self.score(word)
+      score_chart =
+      {
+        1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+        2 => ["D", "G"],
+        3 => ["B", "C", "M", "P"],
+        4 => ["F", "H", "V", "W", "Y"],
+        5 => ["K"],
+        8 => ["J", "X"],
+        10 => ["Q", "Z"]
+      }
+      total = 0
+      i = 0
+      while i < word.length
+        score_chart.each do |number, letters|
+          letters.each do |l|
+            word.each_char do |ch|
+              if l == ch
+                total += number
+              else
+                i += 1
+              end
+            end
+          end
+        end
+      end
+      return total
+    end
   end
 end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -7,10 +7,19 @@ require_relative '../lib/score'
 describe "Score" do
   describe "#initialize" do
     it " should give you an empty array" do
-      words = []
+      # words = []
       my_class = Scrabble::Score.new
       # my_class.must_respond_to :words
       my_class.words.must_be_empty
     end
   end
+
+  describe " self.score method" do
+    it " Check for the short word where the letters don't repeat" do
+      my_class = Scrabble::Score
+      my_class.score("cat").must_equal 5
+    end
+  end
+
+
 end
