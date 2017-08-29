c571390553ea6f089b56870cef5898fc9516615d
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-29 14:17:33 -0700

    Working 50 point bonus and working highest scoring word method.

--- scoring.rb
+++ scoring.rb
@@ -27,22 +27,14 @@ module Scrabble
         total += 50
         # puts "great job, 50 extra points to Ravenclaw!"
       end
-
       return total
     end
 
+    def self.highest_scoring_word(words)
+      words.sort_by! {|word| Scrabble::Scoring.score(word)}
+      return words[-1]
+    end
 
 
   end # end of class
 end # end of module
-
-
-# self.score(word): returns the total score for the given word. The word is input as a string (case insensitive). The chart in the baseline requirements shows the point value for a given letter.
-# Letter	Value
-
-
-# A seven letter word means that a player used all the tiles. Seven letter words receive a 50 point bonus.
-# self.highest_score_from(array_of_words): returns the word in the array with the highest score. In the case of tie, use these tiebreaking rules:
-# It’s better to use fewer tiles, in the case of a tie, prefer the word with the fewest letters.
-# There is a bonus for words that are seven letters. If the top score is tied between multiple words and one used all seven letters, choose the one with seven letters over the one with fewer tiles.
-# If the there are multiple words that are the same score and same length, pick the first one in the supplied list.
--- scoring_spec.rb
+++ scoring_spec.rb
@@ -1,19 +1,46 @@
 require 'minitest/autorun'
+require 'minitest/skip_dsl'
+require 'minitest/reporters'
 require_relative 'scoring'
 
 describe "ScoringClass" do
-  xdescribe 'initiate scoring class' do
+  describe 'initiate scoring class' do
     it "creates an instance of scoring" do
       score = Scrabble::Scoring.new
       score.must_be_instance_of Scrabble::Scoring
     end
+  end
   describe "score method" do
     it 'returns total score for word' do
       score = Scrabble::Scoring.score("cats")
       score.must_equal 6
     end
-
   end
+  describe "all-tile bonus" do
+    it "adds a 50 point bonus if word has 7 letters" do
+      score = Scrabble::Scoring.score("loudest")
+      score.must_equal 58
 
+      newscore = Scrabble::Scoring.score("loudes")
+      newscore.must_equal 7
+    end
+  end
+  describe "highest scoring word" do
+    it "returns the word with the highest score" do
+      array = ["Billy", "Goat", "Nathan", "zzzz"]
+      highest = Scrabble::Scoring.highest_scoring_word(array)
+      highest.must_equal "zzzz"
+    end
+    it "returns the word with the fewest tiles if there is a tie" do
+
+    end
+
+    it "returns the word that uses 7 tiles for bonus if there is a tie" do
+
+    end
+
+    it "returns the first supplied word if there is a tie in score and in length" do
+
+    end
   end
 end
