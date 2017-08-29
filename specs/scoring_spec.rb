ed9dd6ad75a69b5bd6b8cb70ac283e5e720ffefb
enigmagnetic <lauren.cardella@gmail.com>
2017-08-28 17:44:30 -0700

    Add minitests for .score method.

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,1 +1,27 @@
 require_relative 'spec_helper'
+
+
+describe "Scoring" do
+  describe "self.score" do
+    it "returns the total score for the given word" do
+      Scoring.score("hello").must_equal 8
+    end
+
+    it "awards 50 bonus points for a 7-letter word" do
+      Scoring.score("plumped").must_equal 64
+    end
+
+    it "is case-insensitive" do
+      Scoring.score("HELLO").must_equal 8
+      Scoring.score("HeLlO").must_equal 8
+    end
+
+    it "raises an error if the argument is not a string" do
+      proc { Scoring.score("*@?;!^") }.must_raise ArgumentError
+      proc { Scoring.score(2384) }.must_raise ArgumentError
+      proc { Scoring.score("") }.must_raise ArgumentError
+      proc { Scoring.score("2384") }.must_raise ArgumentError
+      proc { Scoring.score(["h", "e", "l", "l", "o"]) }.must_raise ArgumentError
+    end
+  end
+end
