611cd2c17ed480add4acf65a4a235d83e0b6fdde
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-29 14:34:29 -0700

    add player to spec helper

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/scrabble'
-
+require_relative '../lib/player'
 
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
