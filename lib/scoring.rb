2585ac75c6aa2dcd5f44fd1ea237086d77ad6046
Irene DeVera <irenneeyyy@gmail.com>
2017-08-28 17:01:49 -0700

    Add CONSTANT, self.score, and self.highest_score_from

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,20 +1,33 @@
+# require_relative '../wave-1-game'
+
 module Scrabble
   class Scoring
-    attr_reader :score_chart
-    def initialize
-      #TODO: fix structure of hash
-      @score_chart = {"A" => 1, "E" => 1, "I" => 1,"O" => 1, "U" => 1, "L" => 1,  "N" => 1,"R" => 1,"S" => 1,"T" => 1, "D" => 2, "G" => 2,"B" => 3,"C" => 3,  "M" => 3, "P" => 3,   "F" => 4,"H" => 4,"V" => 4,"W" => 4,"Y" => 4,  "K" => 5,"J" => 8,"X" => 8,"Q" => 10, "Z" => 10,
-      }
-    end #initialize
+
+    SCORE_CHART = {"A" => 1, "E" => 1, "I" => 1,"O" => 1, "U" => 1, "L" => 1,  "N" => 1,"R" => 1,"S" => 1,"T" => 1, "D" => 2, "G" => 2,"B" => 3,"C" => 3,  "M" => 3, "P" => 3,   "F" => 4,"H" => 4,"V" => 4,"W" => 4,"Y" => 4,  "K" => 5,"J" => 8,"X" => 8,"Q" => 10, "Z" => 10,
+    }
 
     def self.score(word)
+      word = word.upcase
       word_array = word.split(//)
-      word_score = 0
+      if word_array.length == 7
+        word_score = 50
+      else
+        word_score = 0
+      end
       word_array.each do |letter|
-        word_score += @score_chart[letter]
+        word_score += SCORE_CHART[letter]
       end
       return word_score
     end #def self.score(word)
+
+    def self.highest_score_from(array_of_words)
+      highest_score = 0
+      array_of_words.each do |word|
+        if Scrabble::Scoring.score(word) > highest_score
+          highest_score = Scrabble::Scoring.score(word)
+        end
+      end
+    end
 
   end #Scoring
 end #Scrabble
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -11,14 +11,15 @@ describe "scoring" do
     @score = Scrabble::Scoring.new
   end
 
-  describe "initialize" do
-    it "must be able to initilize @score_chart as a hash" do
-      @score.score_chart.must_be_kind_of Hash
+  describe "SCORE_CHART" do
+    #TODO
+    it "must be able to return SCORE_CHART hash" do
+      Scrabble::Scoring::SCORE_CHART.must_be_kind_of Hash
     end #it "must be able to initilize @score_chart as a hash"
 
-    it "@score_chart must include A-Z" do
+    it "SCORE_CHART must include A-Z" do
       ("A".."Z").each do |letter|
-        @score.score_chart.keys.must_include letter
+        Scrabble::Scoring::SCORE_CHART.keys.must_include letter
       end #.each
     end #it "@score_chart must include ("A".."z")" do
   end #describe "initialize" do
@@ -29,13 +30,26 @@ describe "scoring" do
     end #it "must be a class method" do
 
     it "will return an integer score" do
-      Scrabble::Scoring.score("hell0").must_be_kind_of Integer
+      Scrabble::Scoring.score("HELLO").must_be_kind_of Integer
     end #it "will return an integer score"
 
     it "will return the right score for a word" do
-
+      Scrabble::Scoring.score("CAT").must_equal 5
     end #it "will return the right score for a word" do
 
-
+    it "will add 50 to a 7 letter word" do
+      Scrabble::Scoring.score("CATTTTT").must_equal 59
+    end
   end #describe "self.score(word)" do
+
+  describe "self.highest_score_from(array_of_words)" do
+    it "should return a string" do
+      Scrabble::Scoring.highest_score_from(["cat", "dog", "elephant"]).must_be_kind_of String
+    end
+
+    it "should pick the highest scoring word" do
+      Scrabble::Scoring.highest_score_from(["cat", "elephant"]).must_equal "ELEPHANT"
+    end
+  end # Describe
+
 end #describe "scoring" do
