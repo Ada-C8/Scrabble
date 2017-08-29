8f5c88010a07e612fe7c72a2463c9ef81efc9d1d
Salome Wubeshet <swubeshet@gmail.com>
2017-08-28 17:21:22 -0700

    4th test to spec file, updated the score method for additional bonus points

--- spec/scoring_spec.rb
+++ spec/scoring_spec.rb
@@ -6,19 +6,23 @@ require_relative '../lib/scoring'
 describe "Scoring" do
  before do
   @game = Scoring.new
- end
- describe "initialize" do
+end
+describe "initialize" do
   it "should create a new instance of Scrabble" do
    @game.must_be_instance_of Scoring
-  end
  end
- describe "scoring" do
+end
+describe "scoring" do
   it "should check the value of 1 tile" do
    Scoring.score("A").must_equal 1
-  end
-  it "should check the value of 2 tiles" do
-   Scoring.score("WE").must_equal 5
-  end
  end
+ it "should check the value of 2 tiles" do
+   Scoring.score("WE").must_equal 5
+ end
+ it "should add 50 bonus points to 7 letter word" do
+  Scoring.score("quizzed").must_equal 85
+end
+
+end
 end #end of scoring describe
 
