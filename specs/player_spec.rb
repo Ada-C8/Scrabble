bd19fe1d74b1b620bfec63a2734a268ad67600ae
Amy Cash <amymcash@gmail.com>
2017-08-29 14:30:33 -0700

    Add tests for Initialize, Play, and Total Score

---
+++ specs/player_spec.rb
@@ -0,0 +1,50 @@
+require 'pry'
+require_relative 'spec_helper'
+
+describe "Player class" do
+  before do
+    @bennett = Scrabble::Player.new("Amy")
+  end
+  describe "initialize" do
+    before do
+      @amy = Scrabble::Player.new("Amy")
+    end
+    it "can instantiate a Player class" do
+      Scrabble::Player.new("Bob").must_be_kind_of Scrabble::Player
+    end
+    it "Player can assign and access #name" do
+      @amy.name.must_equal "Amy"
+    end
+
+    it "Can call an empty array on a new player" do
+      @amy.plays.must_be_kind_of Array
+      @amy.plays.must_be_empty
+    end
+  end
+
+  describe "play method" do
+    it "adds word to plays array" do
+      @bennett.play("swift")
+      @bennett.plays.must_equal ["SWIFT"]
+    end
+
+    it "returns the score of the entered word" do
+      @bennett.play("swift").must_equal 11
+    end
+
+    xit "returns false if already won" do
+
+    end
+  end
+
+  describe "total_score method" do
+    it "returns sum of all played words" do
+      @bennett.play("swift")
+      @bennett.play("aaa")
+      @bennett.play("qz")
+
+      @bennett.total_score.must_equal 34
+    end
+  end
+
+end
