82e9aaa9c969429d9c506170335295abf879d0fb
averikitsch <aekitsch@gmail.com>
2017-08-29 13:47:50 -0700

    Wave 2 tests completed

--- specs/scrabble_player_specs.rb
+++ specs/scrabble_player_specs.rb
@@ -10,13 +10,13 @@ describe "Scrabble:Player" do
   end
 
   it "Can respond to name" do
-  @averi.must_respond_to :name
-  @averi.name.must_equal "Averi"
+    @averi.must_respond_to :name
+    @averi.name.must_equal "Averi"
   end
 
   it "plays is an array" do
     @averi.plays.must_be_instance_of Array
-end
+  end
 
   it "plays word adds a word to plays" do
     first_length = @averi.plays.length
@@ -52,7 +52,33 @@ end
     @averi.won?.must_equal false
   end
   it "responds to total_score" do
-
+    @averi.must_respond_to :total_score
+  end
+  it "calculates total_score" do
+    @averi.play("dog")
+    @averi.total_score.must_equal 5
+  end
+  it "calculates total_score for multiple words" do
+    @averi.play("dog")
+    @averi.play("jazzily")
+    @averi.total_score.must_equal (5+85)
+  end
+  it "returns zero for no words played" do
+    @averi.total_score.must_equal 0
+  end
+  it "returns highest scoring word" do
+    @averi.play("dog")
+    @averi.play("jazzily")
+    @averi.highest_scoring_word.must_equal "jazzily"
+  end
+  it "returns highest word score" do
+    @averi.play("dog")
+    @averi.play("jazzily")
+    @averi.highest_word_score.must_equal 85
+  end
+  it "highest scoring word and highest word score work for no input" do
+    @averi.highest_scoring_word.must_equal ""
+    @averi.highest_word_score.must_equal 0
   end
 
 
