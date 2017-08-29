58e4a4d14a5a8a2bbfcb7d62405926691d6ddd0d
Jessica Owens <vertige@gmail.com>
2017-08-29 14:26:05 -0700

    added Todos to player_spec

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,3 +1,21 @@
 require_relative 'spec_helper.rb'
 
 #TODO 11 tests!
+
+
+# TODO `#name`: returns the value of the `@name` instance variable
+
+# TODO `#plays`: returns an Array of the words played by the player
+
+# TODO `#play(word)`: Adds the input word to the `plays` Array
+    # TODO Returns `false` if player has already won
+    # TODO Returns the score of the `word`
+
+# TODO `#total_score`: Returns the sum of scores of played words
+
+# TODO `#won?`: If the player has over 100 points, returns `true`, otherwise returns `false`
+    # TODO  This should be a private method
+
+# TODO  `#highest_scoring_word`: Returns the highest scoring played word
+
+# TODO `#highest_word_score`: Returns the `highest_scoring_word` score
