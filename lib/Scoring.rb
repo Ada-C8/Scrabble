0a7718e55109bf7fef2da6d73cedb0d99adb8b65
Diane Lenertz <lenertzd@grinnell.edu>
2017-08-29 09:42:55 -0700

    make tests pass

--- lib/Scoring.rb
+++ lib/Scoring.rb
@@ -7,9 +7,6 @@ module Scrabble
       A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3, M: 3, P:3,
       F: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10}
       def initialize
-        # @scoring_hash = {
-        # A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3, M: 3, P:3,
-        #   F: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10}
       end
 
       # self.score(word): returns the total score for the given word. The word is input as a string (case insensitive). The chart in the baseline requirements shows the point value for a given letter.
@@ -33,17 +30,34 @@ module Scrabble
       end
 
       def self.highest_score_from(array_of_words)
-        words_hash = {}
         highest_score = 0
         highest_score_word = nil
+        puts "___"
+        puts array_of_words
+        puts "___"
         array_of_words.each do |word|
           score = self.score(word)
-          words_hash[word] = score
+          puts "word #{word}"
+          puts "score #{score}"
+
           if score > highest_score
             highest_score = score
-            highest_score_word = word
+            highest_score_word = word.upcase
+            puts "#{highest_score} *** #{highest_score_word}"
+          elsif score == highest_score && word.length == 7 || highest_score_word.length == 7
+            if word.length == 7
+              highest_score_word = word.upcase
+            # elsif highest_score_word.length == 7
+            #   highest_score_word == w
+      
+          end
+          elsif score == highest_score && word.length < highest_score_word.length
+            highest_score_word = word.upcase
+            puts highest_score_word
+            puts "\n **"
           end
         end
+        return highest_score_word
 
       end
   end
