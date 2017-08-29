1e5fd0a3da7dd9ca83f984fa7b63a70e4218e9ed
Isaac Del Rosario <cecdelr@gmail.com>
2017-08-28 20:01:36 -0700

    Changed letters_equivalent_scoring values into lower case

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -2,19 +2,19 @@ module Scrabble
   class Scoring
 
     def self.score(word)
-      if word.class != String || !word.match?(/^[A-z]*$/) || word.length == 0
+      if word.class != String || !word.match?(/^[A-z]*$/) || word.length == 0 || word.length > 7
         raise ArgumentError.new("Value passed in doesn't contain only letters from A-Z")
       end
-      word = word.upcase
+      word = word.downcase
       word_score = 0
       letters_equivalent_score = {
-        1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-        2 => ["D", "G"],
-        3 => ["B", "C", "M", "P"],
-        4 => ["F", "H", "V", "W", "Y"],
-        5 => ["K"],
-        8 => ["J", "X"],
-        10 => ["Q", "Z"]
+        1 => ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"],
+        2 => ["d", "g"],
+        3 => ["b", "c", "m", "p"],
+        4 => ["f", "h", "v", "w", "y"],
+        5 => ["k"],
+        8 => ["j", "x"],
+        10 => ["q", "z"]
       }
       # Add 50 points if the word is 7 letters
       word_score += 50 if word.length == 7
