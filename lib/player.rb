755ebee199d6ae0c2f160a29c1f9d9454a58b0f9
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-29 14:30:24 -0700

    add comments from assignment sheet

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,7 @@
 module Scrabble
   class Player
+    # name: returns the value of the @name instance variable
+    # plays: returns an Array of the words played by the player
 
     def initialize(name)
       @name = name
@@ -7,19 +9,27 @@ module Scrabble
     end
 
     def play(word)
+      # Adds the input word to the plays Array
+      # Returns false if player has already won
+      # Returns the score of the word
     end
 
     def total_score
+      # total_score: Returns the sum of scores of played words
     end
 
     def highest_scoring_word
+      # highest_scoring_word: Returns the highest scoring played word
     end
 
     def highest_word_score
+      # highest_word_score: Returns the highest_scoring_word score
     end
 
     private
     def won?
+      # won?: If the player has over 100 points, returns true, otherwise returns false
+      # This should be a private method
     end
 
   end # end of Player class
