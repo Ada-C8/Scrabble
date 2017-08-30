d6bbdc696b89cfb289386aaba1e66083ab7d56b6
Tanja Stroble <plusdeltah@gmail.com>
2017-08-30 09:51:32 -0700

    added spec_helperand it works

--- specs/wave-1-spec.rb
+++ specs/wave-1-spec.rb
@@ -1,7 +1,12 @@
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/skip_dsl'
-require_relative '../lib/wave-1-scoring'
+# require 'simplecov'
+# SimpleCov.start
+#
+#
+# require 'minitest/autorun'
+# require 'minitest/reporters'
+# require 'minitest/skip_dsl'
+# require_relative '../lib/wave-1-scoring'
+require_relative 'spec_helper'
 
 describe "Scoring Class" do
   it 'has a hash constant' do
