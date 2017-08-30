575911e757e13a7e7754ec868ae13124c11c4dff
Shaunna <skwiens@gmail.com>
2017-08-29 21:16:56 -0700

    created classes: Player and TileBag

---
+++ lib/wave-2-player.rb
@@ -0,0 +1,24 @@
+module Scrabble
+  class Player
+
+    def initialize
+
+    end
+
+    def play(word)
+    end
+
+    def total_score
+    end
+
+    def won?
+    end
+
+    def highest_scoring_word
+    end
+
+    def highest_word_score
+    end
+
+  end  #end of Player class
+end  #end of Scrabble module
