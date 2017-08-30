f20fbedde92321e3e79e76a0ba73521f374b439f
Lauren <Laurenelee88@gmail.com>
2017-08-30 09:40:46 -0700

    move simplecov to top

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,9 +1,10 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/pride'
-require 'simplecov'
-SimpleCov.start
+
 
 
 require_relative '../lib/scrabble'
