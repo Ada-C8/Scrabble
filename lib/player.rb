9299a0d691c47e61f353b7706693af794cb6190e
hellomarisamorris <hellomarisamorris@gmail.com>
2017-08-30 15:25:12 -0700

    Created private won? method & multiples tests failed.

--- lib/player.rb
+++ lib/player.rb
@@ -1,20 +1,33 @@
 module Scrabble
   class Player
 
-    attr_reader :name, :plays
+    attr_reader :name, :plays, :total
 
     def initialize(input_name)
       @name = input_name
       @plays = []
+      @total = 0
     end
 
     def play(input_word)
+       if won? == true
+         return false
+      end
+      score = Scrabble::Scoring.score(input_word)
       @plays << input_word
-      
+      return score
     end
 
+    def total_score
+      scores = @plays.map{ |word| Scrabble::Scoring.score(word) }
+      @total = scores.inject(:+)
+    end
 
-
+    private
+    def won?
+      total_score
+      @total >= 100 ? true : false
+    end
 
   end # => end of class
 end # => end of module
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -46,14 +46,14 @@ describe "Player Class" do
       test_player = Scrabble::Player.new("Guille")
       test_player.must_respond_to :play
     end
-    it "should return false if player won" do
-      test_player = Scrabble::Player.new("Guillermina")
-      # TODO: make player get 100points
-      test_player.won? = true
-      output = test_player.play("dog")
-      expected_output = false
-      output.must_equal expected_output
-    end
+    # it "should return false if player won" do
+    #   test_player = Scrabble::Player.new("Guillermina")
+    #   # TODO: make player get 100points
+    #   test_player.won? = true
+    #   output = test_player.play("dog")
+    #   expected_output = false
+    #   output.must_equal expected_output
+    # end
     it "should return score of word" do
       test_player = Scrabble::Player.new("Guille")
       output = test_player.play("aaa")
@@ -61,20 +61,55 @@ describe "Player Class" do
       output.must_equal expected_output
     end
   end
+
+  describe "Player.total_score" do
+    it "Should respond to total score" do
+      test_player = Scrabble::Player.new("Guille")
+      test_player.must_respond_to :total_score
+    end
+
+    it "Should return a score of 3 after player plays aaa" do
+      test_player = Scrabble::Player.new("Guille")
+      test_player.play("aaa")
+      expected_output = 3
+      output = test_player.total_score
+      output.must_equal expected_output
+
+    end
+
+    it "Should return sum of scores of multiple played words" do
+      test_player = Scrabble::Player.new("Guille")
+      test_player.play("aaa")
+      test_player.play("eee")
+      expected_output = 6
+      output = test_player.total_score
+      output.must_equal expected_output
+    end
+  end
   #
-  # Describe "Player.total_score" do
+  describe "Player.won?" do
+
+    it "Should respond to .won" do
+      test_player = Scrabble::Player.new("Marisa")
+      test_player.must_respond_to :won?
+
+
+    end
+
+    it "SHould return if false if player has a score less than 100" do
+
+    end
+
+    it "Should return true if player score is greater that or equal to 100" do
+
+    end
+  end
+  #
+  # describe "Player.highest_scoring_word" do
   #
   # end
   #
-  # Describe "Player.won?" do
-  #
-  # end
-  #
-  # Describe "Player.highest_scoring_word" do
-  #
-  # end
-  #
-  # Describe "Player.highest_word_score" do
+  # describe "Player.highest_word_score" do
   #
   # end
 
