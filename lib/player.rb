4999b564b4e74311de60c76576b23bc6b23d6651
Irene DeVera <irenneeyyy@gmail.com>
2017-08-29 21:25:11 -0700

    Add player specs, play(word) method, won? method

--- lib/player.rb
+++ lib/player.rb
@@ -5,14 +5,13 @@
 # Returns false if player has already won
 # Returns the score of the word
 # #total_score: Returns the sum of scores of played words
-# #won?: If the player has over 100 points, returns true, otherwise returns false
-# This should be a private method
+
 # #highest_scoring_word: Returns the highest scoring played word
 # #highest_word_score: Returns the highest_scoring_word score
 
 module Scrabble
   class Player
-    attr_reader :name, :player_words
+    attr_reader :name, :player_words, :total_score
     def initialize(name)
       @name = name
       @player_words = []
@@ -25,10 +24,18 @@ module Scrabble
 
     def play(word)
       @player_words << word
-      #TODO: calculate score of word and add to @total_score (use @total_score in won? method to determine if they have > 100 points)
-      #TODO: return false if player has already won (won? == true)
-      #TODO: else return the score for the word (won? == false)
+      result = Scrabble::Scoring.score(word)
+      @total_score += result
+      if won?
+        return false
+      else
+        return result # Score of the word
+      end
     end #play
+
+    #TODO: calculate score of word and add to @total_score (use @total_score in won? method to determine if they have > 100 points)
+    #TODO: return false if player has already won (won? == true)
+    #TODO: else return the score for the word (won? == false)
 
     #TODO: highest_scoring word --- make a hash of words and their scores and return the highest scoring word? Or run soring method on @player_words to get the highest scoring word (sort by highest score and return last word in array)? BEST IDEA: call highest_score_from_array on @player_words to get the highest scoring word (Scrabble::Scoring.highest_score_from_array(@player_words)) and return the word
 
@@ -36,8 +43,17 @@ module Scrabble
 
 
 
+    private
 
-
+    def won?
+      # won?: If the player has over 100 points, returns true, otherwise returns false
+      # This should be a private method
+      if @total_score > 100
+        return true
+      else
+        return false
+      end
+    end # DEF
 
   end # Player
 end # Scrabble
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -12,19 +12,30 @@ describe "Player" do
   end
 
   describe "#initialize" do
+
     it "will be an instance of player " do
-      Scrabble::Player.new("Mira").must_be_instance_of Scrabble::Player
+      @test_player.must_be_instance_of Scrabble::Player
     end # It "will be an instance of player
 
-    it "will despond to .name" do
-      Scrabble::Player.new("Mira").must_respond_to :name
+    it "will respond to .name" do
+      @test_player.name.must_equal "Mira"
+      @test_player.must_respond_to :name
+
+######TODO: should we test if @name is a string?
+
     end #it "will despond to .name" do
 
-    it "will return the value of @name" do
-      Scrabble::Player.new("Mira").name.must_equal "Mira"
+    it "will respond to .player_words" do
+      @test_player.player_words.must_equal []
+      @test_player.must_respond_to :player_words
     end #it "will return the value of @name" do
 
-    #TODO: should we test if @name is a string?
+    it "will respond to .total_score" do
+      @test_player.total_score.must_be_kind_of Integer
+      @test_player.total_score.must_equal 0
+      # @test_player.total_score.must_respond_to :total_score
+    end #it "will return the value of @name" do
+
   end # Describe "#initialize" do
 
   describe "#plays" do
@@ -34,30 +45,59 @@ describe "Player" do
     end #it "will return an array" do
 
     it "will return an array of strings" do
+
+######TODO: Use an Enumerable here
+
       @test_player.plays.each do |word|
         word.must_be_kind_of String
-    end #.each
-
+      end #.each
     end #it "will return an array of strings" do
+
   end #describe "#plays" do
 
-  describe "#play" do
+  describe "#play(word)" do
 
     it "will add the word to the @player_words array" do
       @test_player.play("hello")
 
       @test_player.player_words.length.must_equal 1
-       array = @test_player.player_words
-       array[0].must_equal "hello"
+      array = @test_player.player_words
+      array[0].must_equal "hello"
     end #it "will add the word to the @player_words array" do
 
-    #TODO: test that it returns false when won? == true
+    it "should add word score to @total_score" do
+      @test_player.play("hello")
+      @test_player.total_score.must_equal 8
+    end
 
-    #TODO: test that it returns a score when won? == false 
+    xit "should respond to won? private method" do
 
-  end #describe "#play" do
+######TODO: Figure out how to test a private method won? after play(word) is ran
+      @test_player.play("hello")
+      expect(@test_player.send(:won?)).to eq(true)
+    end
 
+    xit "if won is true, should return false" do
 
+    end
 
+    xit "if won is false, should return score as Integer" do
+
+    end
+
+  end #describe "#play(word)" do
+
+  xdescribe "won?" do
+
+    xit "should return true if @total_score > 100" do
+      # @total_score = 150
+      # @test_player.won?.must_equal true
+    end # It
+
+    xit "should return false if @total_score < 100" do
+
+    end # It
+
+  end # Describe
 
 end # Describe
