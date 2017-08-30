50bc7feb862c0080152ef7747f2c4fc95e15488f
nkiru onwuneme <generalhrh@gmail.com>
2017-08-30 15:13:13 -0700

    Added plays method.

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,15 @@
-## Don't forget to commit 
+## Don't forget to commit
 
 class Player
   attr_accessor :name
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name  = name
+    @plays = []
+  end
 
+  def plays
+    return @plays
+  end
 
 end # end of the class
