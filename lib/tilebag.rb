6c70a4b2f225a57b584a19e5e0c61ba093ee8f39
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-30 13:51:57 -0700

    add skeleton of tilebag & tilebag_spec

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,6 +1,18 @@
 module Scrabble
   class TileBag
 
+    def initialize
+    end
+
+    #initialize Should set up the instance with a collection of all default tiles
+
+    def draw_tiles(num)
+    end
+    #draw_tiles(num) returns a collection of random tiles, removes the tiles from the default set
+
+    def tiles_remaining(num)
+    end
+    #tiles_remaining returns the number of tiles remaining in the bag
 
   end # end of class
 end # end of module
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,1 +1,29 @@
 require_relative 'spec_helper'
+
+describe 'TileBag' do
+  describe 'initialize' do
+    it 'initializes with a hash of all default tiles' do
+
+    end
+  end
+
+  describe 'draw_tiles' do
+    it 'draws 7 tiles' do
+
+    end
+
+    it 'draws random tiles' do
+
+    end
+
+    it 'does not draw more tiles than exist' do
+
+    end
+  end
+
+  describe 'tiles_remaining' do
+    it 'returns the number of tiles remaining in the bag' do
+      
+    end
+  end
+end
