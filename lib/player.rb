e4a25f8956c5dd4a8f645b73e371455ddda43b09
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-30 12:25:37 -0700

    working plays method & test.

--- lib/player.rb
+++ lib/player.rb
@@ -2,9 +2,17 @@
 module Scrabble
   class Player
 
+  attr_accessor :name
+
     def initialize(name)
       @name = name
     end
+##plays: returns an Array of the words played by the player
+    def plays
+      plays = []
+      return plays
+    end
+
 
 
 
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -4,5 +4,17 @@ describe "PlayerClass" do
   it "initiates an instance of player" do
   player = Scrabble::Player.new("jan")
   player.must_be_instance_of Scrabble::Player
+  end
+
+  it "can access the player's name" do
+    player = Scrabble::Player.new("jan")
+    player.name.must_equal "jan"
+  end
 end
+describe "Plays" do
+  it "returns the word that was played" do
+    play = Scrabble::Player.new("jan")
+    play.plays.must_equal []
+  end
+
 end
