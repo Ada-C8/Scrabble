dcda0ceba8cd0414bf6ac6fcd793365465cc3d14
IsabelDePapel <isuchanek@gmail.com>
2017-08-30 09:32:06 -0700

    Added SimpleCov to spec helper and put .DS_Store in .gitignore

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,9 +1,13 @@
+# SimpleCov needs to be FIRST THING in spec helper
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/scoring'
 require_relative '../lib/player'
 
-
 reporter_options = { color:true}
-Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(reporter_options)
+# Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(reporter_options)
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new(reporter_options)
