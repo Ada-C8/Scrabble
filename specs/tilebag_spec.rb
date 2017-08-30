54bd9612a93727965937570217630c2743793945
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-30 13:54:04 -0700

    test for tilebag TILES hash

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,9 +1,12 @@
 require_relative 'spec_helper'
 
 describe 'TileBag' do
+  before do
+    @tilebag = Scrabble::TileBag.new
+  end
   describe 'initialize' do
     it 'initializes with a hash of all default tiles' do
-
+      @tilebag.TILES.must_be_kind_of Hash
     end
   end
 
@@ -23,7 +26,7 @@ describe 'TileBag' do
 
   describe 'tiles_remaining' do
     it 'returns the number of tiles remaining in the bag' do
-      
+
     end
   end
 end
