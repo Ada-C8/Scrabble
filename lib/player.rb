c7fb364af1181e8823da1e84c5b04db705f7cb32
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-30 14:15:17 -0700

    score of play

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,26 @@
-
+require_relative 'scoring'
 module Scrabble
   class Player
 
-  attr_accessor :name
+  attr_accessor :name, :plays
 
     def initialize(name)
       @name = name
+      @plays = []
     end
 ##plays: returns an Array of the words played by the player
-    def plays
-      plays = []
-      return plays
+    def play(word)
+      plays << word
+      return Scrabble::Scoring.score(word)
     end
+
+
+
+
+
+    ##total_score: Returns the sum of scores of played words
+
+    # Returns false if player has already won
 
 
 
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -12,9 +12,23 @@ describe "PlayerClass" do
   end
 end
 describe "Plays" do
-  it "returns the word that was played" do
+  it "returns an empty array if no words have been played" do
     play = Scrabble::Player.new("jan")
     play.plays.must_equal []
+    play.plays.count.must_equal 0
+    play.plays.must_be_instance_of Array
   end
+
+  it "add word to plays array" do
+    player = Scrabble::Player.new("Jan")
+    player.play("sensitive")
+    player.plays.must_equal ["sensitive"]
+  end
+
+  it "returns the score of the word played" do
+    player = Scrabble::Player.new("Jan")
+    player.play("cat").must_equal 5
+  end
+
 
 end
