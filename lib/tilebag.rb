8d65407e4ee730b1d359896be44409f6f3aaba9c
Sara Frandsen <sara.frandsen@gmail.com>
2017-08-30 14:41:11 -0700

    verify count of each tile available in bag

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,23 +1,26 @@
 module Scrabble
   class TileBag
 
-    TILES = {'A' => 9, 'B' => 2, 'C' => 2, 'D' => 4, 'E' => 12, 'F' => 2,
+    TILES_COUNT = {'A' => 9, 'B' => 2, 'C' => 2, 'D' => 4, 'E' => 12, 'F' => 2,
              'G' => 3, 'H' => 2, 'I' => 9, 'J' => 1, 'K' => 1, 'L' => 4,
              'M' => 2, 'N' => 6, 'O' => 8, 'P' => 2, 'Q' => 1, 'R' => 6,
              'S' => 4, 'T' => 6, 'U' => 4, 'V' => 2, 'W' => 2, 'X' => 1,
              'Y' => 2, 'Z' => 1}
 
     def initialize
+      @bag = []
 
     end
 
     #initialize Should set up the instance with a collection of all default tiles
 
     def draw_tiles(num)
+
     end
     #draw_tiles(num) returns a collection of random tiles, removes the tiles from the default set
 
     def tiles_remaining(num)
+
     end
     #tiles_remaining returns the number of tiles remaining in the bag
 
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -4,23 +4,30 @@ describe 'TileBag' do
   before do
     @tilebag = Scrabble::TileBag
   end
+
   describe 'initialize' do
-    it 'initializes with a hash of all default tiles' do
-      @tilebag::TILES.must_be_kind_of Hash
+    it 'creates a Hash of toal tiles' do
+      @tilebag::TILES_COUNT.must_be_kind_of Hash
+    end
+
+    it 'counts total tiles of each letter in TILES_COUNT' do
+      @tilebag::TILES_COUNT['A'].must_equal 9
     end
   end
 
-  describe 'draw_tiles' do
+  xdescribe 'draw_tiles' do
     it 'draws 7 tiles' do
-
+      @tilebag.draw_tiles(7)
     end
 
     it 'draws random tiles' do
-
+      @tilebag.draw_tiles(7)
+      @tilebag.must_equal rand(TILES)
     end
 
     it 'does not draw more tiles than exist' do
-
+      @tilebag.draw_tiles(num)
+      num.must_not_be_greater_than 7
     end
   end
 
