3428686608df754523474ed5be0388f05cf8cd49
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-30 14:54:37 -0700

    Dry up player spec with instance variable for player one

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,41 +1,33 @@
 require_relative 'spec_helper'
 
 describe "PlayerClass" do
+  before do
+    # SET UP VARIABLES
+  @player1 = Scrabble::PlayerClass.new("Sandy")
+  end
   describe "name instance variable" do
     it "Scrabble::Player responds to name" do
-      # SET UP VARIABLES
-      player1 = Scrabble::PlayerClass.new("Sandy")
-      # CALL THE METHOD BEING TESTED
-      player1.must_respond_to :name
-      # CHECK RESULT OF THAT METHOD
+
+      @player1.must_respond_to :name
     end
     it "returns player's name" do
-      # SET UP VARIABLES
-      player1 = Scrabble::PlayerClass.new("Sandy")
-      # CALL THE METHOD BEING TESTED
-
-      # CHECK RESULT OF THAT METHOD
-      player1.name.must_equal "Sandy"
+      @player1.name.must_equal "Sandy"
     end
   end
   describe "plays method" do
     it "Scrabble::Player responds to Plays" do
-      # SET UP VARIABLES
-      player1 = Scrabble::PlayerClass.new("Sandy")
-      # CALL THE METHOD BEING TESTED
-
-      # CHECK RESULT OF THAT METHOD
-      player1.must_respond_to :plays
+      @player1.must_respond_to :plays
     end
     it "return an array" do
-      player1 = Scrabble::PlayerClass.new("Sandy")
-      player1.plays.must_be_kind_of Array
+      @player1.plays.must_be_kind_of Array
     end
 
     it "returns an array of words" do
-      player1 = Scrabble::PlayerClass.new("Sandy")
-      player1.plays.must_equal ["dog", "kitten", "panda"]
+      @player1.plays.must_equal ["dog", "kitten", "panda"]
     end
+  end
+  describe "Total score method" do
+
   end
 end
 
