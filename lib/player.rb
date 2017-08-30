555e85c08ecfa24fbdd2c4a1c8e588c009e37b89
Sara C <srachandler@gmail.com>
2017-08-30 11:27:04 -0700

    adding player.rb and player spec

---
+++ lib/player.rb
@@ -0,0 +1,45 @@
+require 'scoring'
+
+
+
+module Scrabble
+  class Player
+  attr_reader :name, :plays
+    def initialize(name)
+      @name = name
+    end
+
+    def play(word)
+      @plays = []
+      if won? true
+        return false
+      end
+      @plays << word
+      @word_score = Scrabble::Scoring.score(word)
+      total_score
+      end
+
+    def total_score
+      total_scores = []
+      total_scores << @word_score
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  end
+end
