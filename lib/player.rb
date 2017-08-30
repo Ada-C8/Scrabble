bb5139b525964a0f81911e70be2e8ce7a1e7eeb9
murog <guillermina.muro@gmail.com>
2017-08-30 14:33:05 -0700

    passed plays and part of play testing

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,20 @@
 module Scrabble
   class Player
-    attr_reader :name
+
+    attr_reader :name, :plays
+
     def initialize(input_name)
       @name = input_name
+      @plays = []
     end
+
+    def play(input_word)
+      @plays << input_word
+      
+    end
+
+
+
+
   end # => end of class
 end # => end of module
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -3,10 +3,10 @@ require_relative 'spec_helper'
 describe "Player Class" do
   describe "initializing new Player" do
     it "Should be instance of a new player" do
-      # input_name = "Marisa"
-      # output = Scrabble::Player.new("Marisa")
-      # output.must_be_instance_of Scrabble::Player
-      Scrabble::Player.new("Marisa").must_be_instance_of Scrabble::Player
+      input_name = "Marisa"
+      output = Scrabble::Player.new(input_name)
+      output.must_be_instance_of Scrabble::Player
+      # Scrabble::Player.new("Marisa").must_be_instance_of Scrabble::Player
     end
 
     it "Player should have name attribute" do
@@ -18,18 +18,49 @@ describe "Player Class" do
       output = output_player.name
 
       output.must_equal expected_output
-
     end
 
   end
 
-  # Describe "Player.plays" do
-  #
-  # end
-  #
-  # Describe "Player.play(input_word)" do
-  #
-  # end
+  describe "Player.plays" do
+    it "should respond to plays" do
+      test_player = Scrabble::Player.new("Guille")
+      test_player.must_respond_to :plays
+    end
+    it "should be an array" do
+      test_player = Scrabble::Player.new("Marisa")
+      output = test_player.plays
+      output.must_be_instance_of Array
+    end
+    it "should not be empty after a play" do
+      test_player = Scrabble::Player.new("Marisa")
+      test_player.play("Flo")
+      output = test_player.plays
+      (output.empty?).must_equal false
+    end
+
+  end
+
+  describe "Player.play(input_word)" do
+    it "should respond to play" do
+      test_player = Scrabble::Player.new("Guille")
+      test_player.must_respond_to :play
+    end
+    it "should return false if player won" do
+      test_player = Scrabble::Player.new("Guillermina")
+      # TODO: make player get 100points
+      test_player.won? = true
+      output = test_player.play("dog")
+      expected_output = false
+      output.must_equal expected_output
+    end
+    it "should return score of word" do
+      test_player = Scrabble::Player.new("Guille")
+      output = test_player.play("aaa")
+      expected_output = 3
+      output.must_equal expected_output
+    end
+  end
   #
   # Describe "Player.total_score" do
   #
