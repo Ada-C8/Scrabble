2559e9230ead071c93cc2094f003e7fdddecd7bd
nkiru onwuneme <generalhrh@gmail.com>
2017-08-30 15:01:05 -0700

    Added initialize method to class and corresponding tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,11 @@
+## Don't forget to commit 
+
 class Player
+  attr_accessor :name
 
-
-
-
+def initialize(name)
+  @name = name
 end
+
+
+end # end of the class
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 describe "class Player"  do
 
   before do
-    @player = Player.new
+    @player = Player.new(name)
   end
 
   describe "instance of Player class" do
@@ -12,13 +12,17 @@ describe "class Player"  do
       @player.must_be_instance_of Player
     end
 
-    # it "returns the value of the instance variable" do
-    #   player_1 = Player.
+    it "returns the value of the instance variable" do
 
-    # end
+      @player.must_respond_to :name
+    end
   end
 
+  describe "play method" do
+    it "returns an array of word played by the player" do
 
 
-  
+    end
+  end
+
 end #end of describe
