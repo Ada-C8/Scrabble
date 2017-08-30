543917646ab21721cbee4a6adc3f07db6ec5e38e
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-30 13:37:14 -0700

    update wave-2-game and wave-3-game per corrections

--- lib/wave-3-game.rb
+++ lib/wave-3-game.rb
@@ -5,7 +5,7 @@ module Scrabble
     def initialize
       @words = []
       @players = setup_players
-      @tilebag = Tilebag.new
+      @tilebag = Scrabble::TileBag.new
     end
 
     def play
@@ -27,6 +27,7 @@ module Scrabble
           end
 
           if player_has_won
+            crown_winner(player)
             break
           end
         end
@@ -54,12 +55,6 @@ module Scrabble
 
     def continue?
       return true if @words.length == 0 # haven't started playing yet
-      @players.each do |player|
-        if player.won?
-          crown_winner(player)
-          return false
-        end
-      end
 
       puts "Would you like to play another round? (Y/N)"
       continue = gets.chomp
