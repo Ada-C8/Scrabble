d08e95b6ce0d5ad5e458021fc02bcc3267f883bd
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-08-29 18:12:50 -0700

    Flesh out player class with initialize and play method with tests that pass

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,33 @@
 module Scrabble
   class Player
-    attr_reader :name
+    attr_reader :name, :plays
     def initialize(name)
-    @name = name
-    @plays = []
+      @name = name
+      @plays = []
     end#initialize
 
+    def play(word)
+      if won?
+        return false
+      else
+        @plays << word.upcase
+        return Scrabble::Scoring.score(word)
+      end
+    end #play word end
 
-  end #end class
-end #end module
+    def total_score
+    end #total score end
+
+    def won?
+      false
+    end #won end
+
+    def highest_scoring_word
+    end
+
+    def highest_word_score
+    end
+
+  end # class
+
+end # module
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -9,9 +9,21 @@ describe "Class player" do
     it "Return to name" do
       Scrabble::Player.must_respond_to :name
     end
+
     it "Rspond to new person name" do
       bianca = Scrabble::Player.new("Bianca")
       bianca.name.must_equal "Bianca"
+    end
+
+    it "Player instance variable is empty upon initialization" do
+      Scrabble::Player.new("Mariana").plays.must_equal []
+    end
+
+    describe "play method" do
+
+      it "returns score of word while won is false" do
+        Scrabble::Player.new("Ecuador").play("mama").must_equal 8
+      end
     end
 
 
