1432a866bb4c6f3303a3df48ce9a1be54dbd97e1
Irene DeVera <irenneeyyy@gmail.com>
2017-08-29 17:05:34 -0700

    Added comment

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,5 @@
 # Create a Scrabble::Player class with a minimum of 11 specs. The only required parameter for instances of the class is the player's name. Instances of the class should repond to the following messages (note, this does not necessarily mean that each of these need to be written as new methods):
 #
-# #name: returns the value of the @name instance variable
 # #plays: returns an Array of the words played by the player
 # #play(word): Adds the input word to the plays Array
 # Returns false if player has already won
@@ -18,6 +17,6 @@ module Scrabble
     end
 
 
-    
+
   end # Player
 end # Scrabble
