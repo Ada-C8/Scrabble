fe8510b3b174a73d91ab6a4d62285a9a4f0f14c8
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-31 10:05:53 -0700

    Tilebag initiate & generate tiles. Passing tests.

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -1,14 +1,22 @@
+require 'csv'
 require_relative 'scoring'
 require_relative 'player'
 
 module Scrabble
   class TileBag
+    attr_reader :tiles
 
     def initialize
       @tiles = []
+      create_tilebag
     end
 
-
+    def create_tilebag
+      CSV.open('tiles.csv', "r").each do |row|
+        tile = {row[0] => row[1]}
+        @tiles << tile
+      end
+    end
 
 
 
--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -7,4 +7,11 @@ describe "TileBagClass" do
     tilebag.must_be_instance_of Scrabble::TileBag
   end
 
+  it "contains 26 tiles" do
+    tilebag = Scrabble::TileBag.new
+    tilebag.tiles.must_be_instance_of Array
+    tilebag.tiles.count.must_equal 26
+  end
+
+
 end
