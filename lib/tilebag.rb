021828841aed7e6c0297c1e90c6d4f07ef3e2d49
Tamira Vojnar <tamiravojnar@gmail.com>
2017-08-31 13:33:49 -0700

    Add working draw_tiles method

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -6,15 +6,16 @@ module Scrabble
       @tiles = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
     end
 
+
     def draw_tiles(num)
       raise ArgumentError.new("You must draw 1 or more tiles") if num < 1
       index_array = []
       picked_tiles = []
 
       num.times do
-        last_index = 1 - @tiles.length
-        #TODO: need to find a way to generate a random number between 0 and the last index of the @tiles array
-        #index_array <<  #@tiles.sample.index #rand(0..10)
+        index_array << rand(@tiles.length)
+        #Another way to do this:
+        #index_array << @tiles.index(@tiles.sample)
       end
 
       index_array.each do |index|
