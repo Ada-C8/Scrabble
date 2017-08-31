7447e974a82a9e95e87d00c93618158d4a4e7772
Julia <julia.meier@gmail.com>
2017-08-31 10:28:13 -0700

    created tilebag_spec file with initial framework

---
+++ specs/tilebag_spec.rb
@@ -0,0 +1,30 @@
+require_relative 'spec_helper.rb'
+
+describe "The Tilebag class" do
+
+  before do
+    @tilebag = Scrabble::Tilebag.new
+  end
+
+  describe "Initializing Tilebag" do
+
+  end
+
+  describe "The draw_tiles method" do
+
+    it "Responds to the draw_tiles method" do
+
+    end
+
+  end
+
+  describe "The tiles_remaining method" do
+
+    it "Responds to the tiles_remaining method" do
+
+    end
+
+  end
+
+
+end
