f5a011f7e9a282f785b003b512b2bcf75ec8253d
Diane Lenertz <lenertzd@grinnell.edu>
2017-08-30 21:35:10 -0700

    make Ruby style guide adjustments

--- lib/Player.rb
+++ lib/Player.rb
@@ -2,7 +2,8 @@ require_relative 'Scoring'
 
 module Scrabble
   class Player
-    attr_reader :name, :plays, :total_score, :highest_scoring_word, :highest_word_score
+    attr_reader :name, :plays, :total_score, :highest_scoring_word,
+                :highest_word_score, :tiles
 
     def initialize(name)
       @name = name
@@ -12,33 +13,30 @@ module Scrabble
       @highest_word_score = 0
     end
 
-
-    #plays returns an Array of the words played by the player
     def play(word)
-      if won? == false #adds input word to plays Array
-      @plays << word
-      @total_score += Scoring.score(word)
+      if won? == false
+        @plays << word
+        @total_score += Scoring.score(word)
       else
-      return false
+        return false
       end
-      #returns false if the player has already won
-      #won?
     end
 
     def highest_scoring_word
       @highest_scoring_word = Scoring.highest_score_from(@plays)
-      return @highest_scoring_word
     end
 
     def highest_word_score
       highest_scoring_word
       @highest_word_score = Scoring.score(@highest_scoring_word)
-
-      return @highest_word_score
     end
 
+    def draw_tiles(num)
+      @tiles = Scrabble::Tilebag.draw_tiles(num)
+    end
 
     private
+
     def won?
       if @total_score > 100
         return true
@@ -46,11 +44,5 @@ module Scrabble
         return false
       end
     end
-
-
-
-
-
-
   end
 end
