57683a000fe08d8b510bb883fa08b784c0a70e80
Diane Lenertz <lenertzd@grinnell.edu>
2017-09-01 14:10:51 -0700

    initialize and add check_word method

--- lib/Dictionary.rb
+++ lib/Dictionary.rb
@@ -2,7 +2,18 @@
 module Scrabble
   class Dictionary
 
+    def initialize
+    end
 
-
+    def check_word(word)
+      word = word.downcase
+      File.open('lib/engmix.txt').each do |line|
+        line = line[0..line.length-2]
+        if line == word
+          return true
+        end
+      end
+      return false
+    end
   end
 end
