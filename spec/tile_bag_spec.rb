9a266378ab571192105483fe4152cf71ede8093d
nkiru onwuneme <generalhrh@gmail.com>
2017-09-01 11:33:14 -0700

    Added tests for draw tile method

--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -1,8 +1,32 @@
 describe "class TileBag" do
+  before do
+   @tile_bag = Scrabble::TileBag.new
+ end
+
   describe "initialize" do
     it "sets up the instance with a collection of all default tiles" do
-      rack = Scrabble::TileBag.new
-      rack.must_be_instance_of Scrabble::TileBag
+      @tile_bag.must_be_instance_of Scrabble::TileBag
     end
   end
-end
+
+  describe "draw tiles" do
+    it "returns nil if input is more than tilebag" do
+      @tile_bag.draw_tiles(100).must_equal nil
+    end
+    it "returns an array of tiles to user" do
+      @tile_bag.draw_tiles(3).must_be_kind_of Array
+    end
+  end
+
+
+    # it "removes the tiles from the default set" do
+    #
+    # end
+
+end #end of describe block
+
+
+# returns a collection of random tiles, removes the tiles from the default set
+
+
+#
