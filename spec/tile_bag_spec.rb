f0ce81d6ae80ba8ab4997fb934297678332152c2
nkiru onwuneme <generalhrh@gmail.com>
2017-09-01 12:10:57 -0700

    Added tests for tiles remaining

--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -11,16 +11,16 @@ describe "class TileBag" do
 
   describe "draw tiles" do
     it "returns nil if input is more than tilebag" do
-      @tile_bag.draw_tiles(100).must_equal nil
+      @tile_bag.draw_tiles(100).must_be_nil true
     end
 
     it "returns an array of tiles to user" do
       @tile_bag.draw_tiles(3).must_be_kind_of Array
     end
 
-    # it "return an empty array of tiles to user" do
-    #   @tile_bag.draw_tile
-    # end
+    it "returns an empty array of tiles to user" do
+      @tile_bag.draw_tiles(0).must_be_empty Array
+    end
 
   end
 
@@ -28,6 +28,10 @@ describe "class TileBag" do
     it "subtracts from the values of letter quantity and returns remaining tiles" do
       @tile_bag.draw_tiles(5)
       @tile_bag.tiles_remaining.must_equal 93
+    end
+    it "returns the total letter quantity when no tile(s) is drawn" do
+      @tile_bag.draw_tiles(0)
+      @tile_bag.tiles_remaining.must_equal 98
     end
   end
 
