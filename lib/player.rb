b2b37ee5146de49c6d2e36db9ff94ff2871aa6a5
Canaan West <canaan.west@gmail.com>
2017-09-01 13:19:17 -0700

    player got a draw_tiles method with some helper methods

--- lib/player.rb
+++ lib/player.rb
@@ -6,10 +6,12 @@ require 'tilebag'
 module Scrabble
   class Player
   attr_reader :name, :plays, :word_scores
-    def initialize(name)
+    def initialize(name, tile_bag)
       @name = name
+      @tile_bag = tile_bag
       @word_scores = []
       @plays = []
+      @hand = []
     end
 
     def play(word)
@@ -37,6 +39,30 @@ module Scrabble
 
     def highest_word_score
       return Scrabble::Scoring.score(highest_scoring_word)
+    end
+
+    def draw_tiles
+      if bag_empty?
+        return "Sorry, you can't draw from an empty bag! Work with whatcha momma gave ya!"
+      end
+
+      num = 7 - @hand.length
+      if num > @tile_bag.num_tiles_remaining
+        num = @tile_bag.num_tiles_remaining
+      end
+
+      push_to_hand(@tile_bag.draw_tiles(num))
+    end
+
+    def bag_empty?
+      if @tilebag.num_tiles_remaining == 0
+        return true
+      end
+    end
+
+    def push_to_hand(tiles_you_drew)
+      @hand.push(tiles_you_drew)
+      @hand.flatten
     end
 
 private
