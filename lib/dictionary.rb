348d321b5187bfc96fcb27eee39cc48434ce7fb1
Jessica Owens <vertige@gmail.com>
2017-08-31 18:44:17 -0700

    Passed first test of Dictionary initialization. Q: why a non self method works v a self method?

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -6,23 +6,27 @@ module Scrabble
 
     SCRABBLE_DICT = CSV.read('./support/dictionary.csv')
 
-    def initialize
-      @dictionary_hash = self.import(SCRABBLE_DICT)
-    end
-
-    def self.import(csv)
+    def import(csv)
       hash = {}
-      csv.each.do |word_row|
+
+      csv.each do |word_row|
         word = word_row[0]
         letter1 = word[0].to_sym
-        if hash.key?(letter1)
+
+        if !hash.key?(letter1)
           hash[letter1] = [word]
         else
           hash[letter1] << word
         end
+      end
+
       return hash
     end
 
+
+    def initialize
+      @dictionary_hash = self.import(SCRABBLE_DICT)
+    end
 
 
   end #end of Dictionary clas
--- specs/dictionary_spec.rb
+++ specs/dictionary_spec.rb
@@ -4,7 +4,7 @@ describe "Dictionary" do
   it "Can create a Dictionary hash with first letter as key and values as arrays of words beginning with key" do
     dict = Scrabble::Dictionary.new
 
-    dict.must_be_instance_of Dictionary
+    dict.must_be_instance_of Scrabble::Dictionary
     dict.dictionary_hash.must_be_kind_of Hash
 
   end
