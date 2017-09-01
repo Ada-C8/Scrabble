e4fce50db2818e2ced1040435083357080b32beb
sairagula <abdukhalieva@gmail.com>
2017-09-01 16:07:46 -0700

    after simplecove coverage check

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 
+ require 'simplecov'
+ SimpleCov.start
  require 'minitest'
  require 'minitest/autorun'
  require 'minitest/reporters'
- require 'simplecov'
+
  require_relative '../lib/player'
  require_relative '../lib/score'
  require_relative '../lib/tilebag'
- SimpleCov.start
