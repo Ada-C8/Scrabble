732d000763bc292458e7d663095b45cb5c27b473
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-04 04:24:32 -0700

    Changed back to use original Wave 2 Player so player_specs pass

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,6 @@
 require_relative 'scoring'
-# require_relative 'player' # Wave 2
-require_relative 'player_optional' # Does not work with player_specs but has optional play(word) requirement
+require_relative 'player' # Wave 2
+# require_relative 'player_optional' # Does not work with player_specs but has optional play(word) requirement
 require_relative 'tilebag' # Wave 3
 
 require_relative 'board' # Optional
